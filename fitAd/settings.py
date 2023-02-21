9350c5fc45fccbda050886894bee6f4408f02aaf
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-02-21 16:17:58 +0100

    Added the `room` app to the installed apps list

--- fitAd/settings.py
+++ fitAd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local APPs
+    "room.apps.RoomConfig",
 ]
 
 MIDDLEWARE = [
