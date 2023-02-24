00056fe9e98340ae6639ad4f2ce0ba70e8b00906
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-02-25 00:16:02 +0100

    Created serializers for the API viewsets

---
+++ rooms/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+# Local imports
+from .models import Room, Requests, Member
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "id",
+            "code",
+            "host",
+            "created_time",
+        )
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ()
+
+class JoinRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "code",
+        )
+
+class RequestsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = (
+            "song_title",
+            "artiste",
+        )
