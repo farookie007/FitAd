d4299738b17e32cb23596e9ed8e549507a10b838
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-04-29 00:34:57 +0100

    Refined the permissions conditionals

--- api/permissions.py
+++ api/permissions.py
@@ -1,23 +1,18 @@
 from rest_framework import permissions
 
-    
+
 class IsHostPermission(permissions.BasePermission):
-    """Permission for the host of the Room object."""
+    """Permission fo the host of the `Room` object."""
+
     def has_object_permission(self, request, view, obj):
-        """Only the host has this permission"""
-        return obj.host == request.session.session_key
-
-
-class IsNotHostPermission(permissions.BasePermission):
-    """Permission not for the host of the Room object."""
-    def has_object_permission(self, request, view, obj):
-        """Only the host has this permission"""
-        return obj.host != request.session.session_key
-
-
+        """Only the `host` of the `Room` is permitted."""
+        return bool(obj.host == request.session.session_key)
+    
 class IsMemberPermission(permissions.BasePermission):
-    """Permission for the Members in the Room (including the 
-    host)."""
+    """Permission for the members of the `Room`."""
     def has_object_permission(self, request, view, obj):
-        """Only the host has this permission"""
-        return (obj.host == request.session.session_key) or (request.session.get("room_code") == obj.code)
+        """Only the `host` and member of the `Room` has this permission."""
+        is_host = (obj.host == request.session.session_key)
+        is_member = (request.session.get("room_code") == obj.code)
+        print(f"[PERMISSION]: \n\tis_host = {is_host}\n\tis_member = {is_member}")
+        return bool(is_host or is_member)
